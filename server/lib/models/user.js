1340a09442ec0df9321223a69415f4d0e10b2a67
Gloria Guy <totallygloria@gmail.com>
2017-01-02 11:00:45 -0800

    Adds a mongoose user model with bcrypt for passwords
    Model includes empty object types for github, linkedIn, and twitter
    so that we can add OAuth data to the central log in info as the app
    grows. Might need to change the types if empty object {} doesn't
    work.

---
+++ server/lib/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  location: String,
+  github: { type: {} },
+  linkedIn: { type: {} },
+  twitter: { type: {} }
+
+});
+
+
+userSchema.methods.generateHash = function( password ) {
+  return this.password = bcrypt.hashSync( password, 10 );
+};
+
+userSchema.methods.compareHash = function( password ) {
+  return bcrypt.compareSync( password, this.password );
+};
+
+
+module.exports = mongoose.model('User', userSchema);
+

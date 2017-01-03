01e90367b9c8a7a8e60796bf9d2abb8e18372ba9
Gloria Guy <totallygloria@gmail.com>
2017-01-03 09:48:59 -0800

    Adds ng-dialog package to app to control sign-in/sign-up window.

--- app/src/app.js
+++ app/src/app.js
@@ -3,13 +3,17 @@ import components from './components';
 import services from './services';
 
 import uiRouter from 'angular-ui-router';
+import dialog from 'ng-dialog';
+import 'ng-dialog/css/ngDialog.css';
+import 'ng-dialog/css/ngDialog-theme-default.css';
 
 import routes from './routes';
 
 const app = angular.module('TechFolio', [
   components,
   services,
-  uiRouter
+  uiRouter,
+  dialog
 ]);
 
 app.value('apiUrl', 'http://localhost:3500');

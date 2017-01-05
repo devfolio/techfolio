8aae75536aebcccd8caf7557b753b0c791f63555
Gloria Guy <totallygloria@gmail.com>
2017-01-05 13:37:32 -0800

    Updates styling for home page to have menu bar, boxes, sign in / out
    The home component is now the default when you hit / instead of
    being at /home. It has most of the styling in the component instead
    of in the main, which will have to move when we move the menu bar
    to index.html.
    
    The sign in and out button toggles depending on if the user has a
    token saved or not. Signing in takes a user to their profile page.

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -6,6 +6,24 @@ export default {
   controller
 };
 
-function controller() { 
+controller.$inject = ['userService', 'tokenService', '$state'];
+
+function controller(userService, tokenService, $state) {
   this.styles = styles;
+
+  this.$onInit = () => {
+    if (tokenService.get()) {
+      this.hastoken = true;
+    }
+  };
+
+  this.signin = () => {
+    $state.go('userDash');
+  };
+
+  this.signout = () => {
+    userService.signout();
+    this.hastoken = false;
+    $state.go('home');
+  };
 }

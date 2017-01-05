68b264fddf7e3406e01a305415ca8b031e6e33a4
Gloria Guy <totallygloria@gmail.com>
2017-01-05 14:58:53 -0800

    Moves all of the nav controls/styles out of home into a component
    So the home and profile page can use the same nav bar, moved all of
    it's styling into main, and then moved the controller and template
    bits into a separate nav component.
    
    The home and profile routes _dont_ have named views, instead, they
    include the nav as a component directly.

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -6,24 +6,6 @@ export default {
   controller
 };
 
-controller.$inject = ['userService', 'tokenService', '$state'];
-
-function controller(userService, tokenService, $state) {
+function controller() {
   this.styles = styles;
-
-  this.$onInit = () => {
-    if (tokenService.get()) {
-      this.hastoken = true;
-    }
-  };
-
-  this.signin = () => {
-    $state.go('userDash');
-  };
-
-  this.signout = () => {
-    userService.signout();
-    this.hastoken = false;
-    $state.go('home');
-  };
 }

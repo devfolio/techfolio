0149cca4d6f795078d2b578eccf12e00a42074f5
Gloria Guy <totallygloria@gmail.com>
2017-02-10 12:35:54 -0800

    Cleans up router require, fixes spacing
    Bringing in all of Express in a require when we only need the router
    seemed like over-kill. Simlifies into one import.

--- server/lib/routes/admin-router.js
+++ server/lib/routes/admin-router.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const User = require('../models/user');
 
 router
-  .get('/', (req,res,next) => {
+  .get('/', (req, res, next) => {
     User.find({})
     .then(users => res.send(users))
     .catch(next);

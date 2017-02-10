f6cd799195cef1fc26b9cfb2d614bc89264d6daa
Gloria Guy <totallygloria@gmail.com>
2017-02-10 15:47:30 -0800

    Adds ensureToken, ensureAdmin middleware to admin routes to start the authorization checks
    The token now includes putting user's roles in an array on their
    token. This roles array is checked for an 'admin' entry, if found
    it passes back to the route, otherwise it throws a 401 error.
    
    Using a plain-text array of roles is almost certainly a terrible
    idea. This is just to get the structure up and running before
    getting authorization working.

--- server/lib/routes/admin-router.js
+++ server/lib/routes/admin-router.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const ensureToken = require('../auth/ensure-token')();
+const ensureAdmin = require('../auth/ensure-admin')();
 
 router
-  .get('/', (req, res, next) => {
+  .get('/', ensureToken, ensureAdmin, (req, res, next) => {
     User.find({})
-    .then(users => res.send(users))
-    .catch(next);
+      .then(users => res.send(users))
+      .catch(next);
   })
 
-  .delete('/:id', (req, res, next) => {
+  .delete('/:id', ensureToken, ensureAdmin, (req, res, next) => {
     User.findByIdAndRemove(req.params.id)
     .then(deleted => res.send(deleted))
     .catch(next);

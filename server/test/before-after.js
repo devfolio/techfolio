cf8d9093e6a8b1644babbaf8c24d4596f5d2a093
Gloria Guy <totallygloria@gmail.com>
2017-02-10 15:45:45 -0800

    Configs tests to use testDB instead of production dbURI
    Importing in the production config file was pointing the db to use
    the production dbURI, instead of creating a test db. This puts all
    of the mongo/mongoose configs into the tests and then creates and
    drops the test db before/after each test. Working.

--- server/test/before-after.js
+++ server/test/before-after.js
@@ -2,16 +2,20 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-const connection = require('../lib/setup-mongoose');
+// Set up mongoDB
+const dbURI = 'mongodb://localhost:27017/testDB';
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect(dbURI);
+const connection = mongoose.connection;
 
-
-before(done => {
+before( done => {
   const drop = () => connection.db.dropDatabase(done);
   if (connection.readyState === 1) drop();
-  else connection.on('open', drop);
+  else connection.on( 'open', drop );
 });
 
-after(done => {
+after( done => {
   const drop = () => connection.db.dropDatabase(done);
   if (connection.readyState === 1) drop();
   else connection.on( 'open', drop );
